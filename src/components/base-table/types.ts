4656b1a45285ad46a6ef9958375160443fc1ffa5
AliTaee <ali.taee.72@gmail.com>
2024-10-26 12:24:36 +0200

    setup: import base components from shadcn

---
+++ src/components/base-table/types.ts
@@ -0,0 +1,9 @@
+interface TableHeadProps extends React.ThHTMLAttributes<HTMLTableCellElement> {
+  className?: string
+}
+
+interface TableCellProps extends React.TdHTMLAttributes<HTMLTableCellElement> {
+  className?: string
+}
+
+export type { TableHeadProps, TableCellProps }
